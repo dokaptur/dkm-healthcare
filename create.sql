c69317bafd50e2043e1a3eb6cc89656ffbda8aec
mike1729 <michalswietek88@gmail.com>
2014-01-28 20:16:38 +0100

    update create-,insert.sql

--- create.sql
+++ create.sql
@@ -6,6 +6,14 @@ CREATE SEQUENCE "dkm-healthcare".seq_nr_recepty START WITH 1 INCREMENT BY 1;
 
 CREATE SEQUENCE "dkm-healthcare".seq_nr_skier START WITH 1 INCREMENT BY 1;
 
+CREATE SEQUENCE "dkm-healthcare".seq_id_osoby_leki START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "dkm-healthcare".seq_id_osoby_alergie START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "dkm-healthcare".seq_id_osoby_info START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE "dkm-healthcare".seq_id_osoby_historia START WITH 1 INCREMENT BY 1;
+
 
 CREATE TABLE "dkm-healthcare".osoby ( 
 	pesel                char(11)  NOT NULL,
@@ -15,12 +23,26 @@ CREATE TABLE "dkm-healthcare".osoby (
 	imie                 varchar(20)  NOT NULL,
 	nazwisko             varchar(50)  NOT NULL,
 	data_urodzenia       date  NOT NULL,
+	lekarz_rodzinny      char(11)  ,
+	historia_modyfikacja date DEFAULT current_date NOT NULL,
 	adres                text  ,
 	aptekarz             bool  ,
 	admin                bool DEFAULT 'false' NOT NULL,
 	email                text  ,
 	CONSTRAINT pk_osoby PRIMARY KEY ( pesel )
  );
+ 
+ CREATE TABLE "dkm-healthcare".lekarze ( 
+	id_lekarz            char(11)  NOT NULL,
+	prawa                bool DEFAULT 'true' NOT NULL,
+	CONSTRAINT pk_lekarze PRIMARY KEY ( id_lekarz ),
+	CONSTRAINT fk_lekarze FOREIGN KEY ( id_lekarz ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE
+ );
+
+ALTER TABLE "dkm-healthcare".osoby add CONSTRAINT fk1_osoby FOREIGN KEY ( lekarz_rodzinny ) REFERENCES "dkm-healthcare".lekarze( id_lekarz ) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE "dkm-healthcare".osoby ADD CONSTRAINT ck_1 CHECK ( pesel != lekarz_rodzinny );
+
 
 CREATE TABLE "dkm-healthcare".placowki ( 
 	id_placowka          numeric  NOT NULL,
@@ -46,41 +68,45 @@ CREATE TABLE "dkm-healthcare".zabiegi (
 	CONSTRAINT pk_zabiegi PRIMARY KEY ( id_zabieg )
  );
 
-CREATE TABLE "dkm-healthcare".lekarze ( 
-	id_lekarz            char(11)  NOT NULL,
-	prawa                bool DEFAULT 'true' NOT NULL,
-	CONSTRAINT pk_lekarze PRIMARY KEY ( id_lekarz ),
-	CONSTRAINT fk_lekarze FOREIGN KEY ( id_lekarz ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE
- );
 
 CREATE TABLE "dkm-healthcare".osoby_info ( 
+        id                   int DEFAULT nextval('"dkm-healthcare".seq_id_osoby_info'),
 	pesel                char(11)  NOT NULL,
-	lekarz_rodzinny      char(11)  ,
-	historia_modyfikacja date DEFAULT current_date NOT NULL,
 	info                 text  ,
-	CONSTRAINT pk_osoby_info PRIMARY KEY ( pesel ),
-	CONSTRAINT fk_osoby_info FOREIGN KEY ( pesel ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT fk1_osoby_info FOREIGN KEY ( lekarz_rodzinny ) REFERENCES "dkm-healthcare".lekarze( id_lekarz ) ON DELETE SET NULL ON UPDATE CASCADE
+	aktualne             bool,
+	CONSTRAINT pk_osoby_info PRIMARY KEY ( pesel, info ),
+	CONSTRAINT fk_osoby_info FOREIGN KEY ( pesel ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE
  );
  
- ALTER TABLE "dkm-healthcare".osoby_info ADD CONSTRAINT ck_1 CHECK ( pesel != lekarz_rodzinny );
-
- 
  CREATE TABLE "dkm-healthcare".osoby_alergie ( 
+ 	id		     int DEFAULT nextval('"dkm-healthcare".seq_id_osoby_alergie'),
 	pesel                char(11)  NOT NULL,
 	alergia              text  ,
+	aktualne             bool NOT NULL,
 	CONSTRAINT pk_osoby_alergie PRIMARY KEY ( pesel, alergia ),
 	CONSTRAINT fk_osoby_alergie FOREIGN KEY ( pesel ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE
  );
  
  CREATE TABLE "dkm-healthcare".osoby_leki ( 
+        id                   int DEFAULT nextval('"dkm-healthcare".seq_id_osoby_leki'),
 	pesel                char(11)  NOT NULL,
 	lek                  text  ,
+	od                   date NOT NULL DEFAULT current_date,
+	"do"                 date NOT NULL,
 	CONSTRAINT pk_osoby_leki PRIMARY KEY ( pesel, lek ),
 	CONSTRAINT fk_osoby_leki FOREIGN KEY ( pesel ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE
  );
+ 
+ ALTER TABLE "dkm-healthcare".osoby_leki ADD CONSTRAINT ck_1 CHECK ( od < "do" );
 
-
+ CREATE TABLE "dkm-healthcare".osoby_historia ( 
+        id                   int DEFAULT nextval('"dkm-healthcare".seq_id_osoby_historia'),
+	pesel                char(11)  NOT NULL,
+	plik                 bytea,
+	nazwa                varchar(200),
+	CONSTRAINT pk_osoby_historia PRIMARY KEY ( pesel ),
+	CONSTRAINT fk_osoby_leki FOREIGN KEY ( pesel ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE
+ );
 
 --CREATE INDEX idx_osoby_info ON "dkm-healthcare".osoby ( lekarz_rodzinny );
 
@@ -134,11 +160,15 @@ CREATE TABLE "dkm-healthcare".skierowania (
 	id_lekarz            char(11)  ,
 	id_zabieg            integer  NOT NULL,
 	zrealizowany         bool DEFAULT 'false' NOT NULL,
+	od                   date NOT NULL DEFAULT current_date,
+	"do"                 date NOT NULL,	
 	CONSTRAINT pk_skierowania PRIMARY KEY ( numer ),
 	CONSTRAINT fk_skierowania_1 FOREIGN KEY ( id_zabieg ) REFERENCES "dkm-healthcare".zabiegi( id_zabieg ) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT fk_skierowania FOREIGN KEY ( pesel ) REFERENCES "dkm-healthcare".osoby( pesel ) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT fk_skierowania_0 FOREIGN KEY ( id_lekarz ) REFERENCES "dkm-healthcare".lekarze( id_lekarz ) ON DELETE SET NULL ON UPDATE CASCADE
  );
+ 
+ALTER TABLE "dkm-healthcare".skierowania ADD CONSTRAINT ck_1 CHECK ( od < "do" );
 
 CREATE INDEX idx_skierowania ON "dkm-healthcare".skierowania ( pesel );
 
@@ -184,13 +214,6 @@ CREATE VIEW "dkm-healthcare".zabiegi_info AS SELECT z.nazwa, id_zabieg ,adres, m
 join "dkm-healthcare".placowki p using (id_placowka) order by id_zabieg;
 
 
-
-create or replace function add_osoba_info() returns trigger as $$
-begin
-    insert into "dkm-healthcare".osoby_info(pesel) values (new.pesel);
-    return new;
-end;
-$$ language plpgsql;;
 
 create or replace function change_pesel_check() returns trigger as $$
 begin
@@ -261,10 +284,6 @@ begin
 end;
 $$ language plpgsql;;
 
-
-
-create trigger add_osoba_info after insert on "dkm-healthcare".osoby for row
-execute procedure add_osoba_info();;
 
 create trigger change_pesel_check before update on "dkm-healthcare".osoby for row
 execute procedure change_pesel_check();;
